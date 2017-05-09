e3acc03e9018cc7c448ddfed79e840a2dc9f5ee0
sajibu3108 <sajibu3108@gmail.com>
2017-05-09 16:25:02 -0600

    Version1

--- Design01.cydsn/codegentemp/Design01.ctl
+++ Design01.cydsn/codegentemp/Design01.ctl
@@ -1,6 +1,6 @@
 -- ======================================================================
 -- Design01.ctl generated from Design01
--- 05/09/2017 at 10:31
+-- 05/09/2017 at 16:14
 -- This file is auto generated. ANY EDITS YOU MAKE MAY BE LOST WHEN THIS FILE IS REGENERATED!!!
 -- ======================================================================
 
--- Design01.cydsn/main.c
+++ Design01.cydsn/main.c
@@ -18,6 +18,7 @@
 int16 data = 0;
 float volts = 0;
 float angulo = 0.0;
+int   ContInt=0;
 
 int value_counter = 0;
 float distancia = 0;
@@ -25,9 +26,8 @@ char imp[9];
 
 float tiempo = 0.0;
 
-char displayStr[16] = {'\0'};
-char displayStrang[16] = {'\0'};
-char displayStrdist[16] = {'\0'};
+char displayStrLCD[16] = {'\0'};
+char displayStrUART[16] = {'\0'};
 
 /* Interrupt prototype */
 CY_ISR_PROTO(isr_10_Handler);
@@ -36,18 +36,24 @@ CY_ISR_PROTO(isr_10_Handler);
 CY_ISR(isr_10_Handler)
 {
     // Denotes interrupt begin
+
     ADC_IsEndConversion(ADC_WAIT_FOR_RESULT);
     data = ADC_GetResult16();
     volts = ADC_CountsTo_Volts(data);
-    angulo = (15.523*volts - 34.891)*3.1416/180;
+    angulo = (15.523*volts - 34.891)*3.1416/180;    
     
-    Trigger_out_Write(1);
-    CyDelayUs(10);
-    Trigger_out_Write(0);
-    CyDelayUs(1000);
-    while (echo_Read()==1){}
-    value_counter=65535-Timer2_ReadCounter();
-    distancia=value_counter/58.0;
+    if (ContInt>10){
+        Trigger_out_Write(1);
+        CyDelayUs(10);
+        Trigger_out_Write(0);
+        CyDelayUs(1000);
+        while (echo_Read()==1){}
+        value_counter=65535-Timer2_ReadCounter();
+        distancia=value_counter/58.0;
+        ContInt=0;
+    }
+    ContInt++;
+   
 }
 
 int main(void)
@@ -71,24 +77,21 @@ int main(void)
     LCD_PrintString("Angulo - Dist");
     
     UART_Start();
-    sprintf(displayStr,"%.2f r %.1f cm",volts, distancia);
+    sprintf(displayStrLCD,"%.2f r %.1f cm",volts, distancia);
     LCD_Position(1,0);
-    LCD_PrintString(displayStr);
+    LCD_PrintString(displayStrLCD);
         
-    UART_PutString(displayStr);
+    UART_PutString(displayStrUART);
     UART_PutString("\n\r");    
     // Infinite loop
     for(;;)
     {  
-       sprintf(displayStr,"%.2f r %.1f cm",volts, distancia);
-       sprintf(displayStrang,"%.2f",angulo);
-       sprintf(displayStrdist,"%.1f",distancia);
+       sprintf(displayStrLCD,"%.2f r %.1f cm",volts, distancia);
+       sprintf(displayStrUART,"%.2f, %.1f",angulo,distancia);
        LCD_Position(1,0);
-       LCD_PrintString(displayStr);
+       LCD_PrintString(displayStrLCD);
     
-       UART_PutString(displayStrang);
-       UART_PutString("\n\r");
-       UART_PutString(displayStrdist);
+       UART_PutString(displayStrUART);
        UART_PutString("\n\r");
     } // for
 
